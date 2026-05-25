e9655af40d48b21b50c6dc3f0fe6d2dfef12df23
Gautam <gk460273@gmail.com>
2026-05-25 14:11:57 +0530

    Fixed duplicate teacher dashboard route

--- app.py
+++ app.py
@@ -3490,14 +3490,46 @@ def teacher_login():
         flash('Invalid credentials.', 'error')
     return render_template('teacher_login.html')
 
+
+
 @app.route('/teacher/dashboard')
 def teacher_dashboard():
-    if session.get('role') != 'teacher': return redirect('/teacher/login')
+
+    if session.get('role') != 'teacher':
+        return redirect('/teacher/login')
+
     db = get_db()
-    classes  = db.execute('SELECT DISTINCT class_name FROM students ORDER BY class_name').fetchall()
-    students = db.execute('SELECT * FROM students ORDER BY class_name, roll_number').fetchall()
-    homework = db.execute('SELECT h.*,t.name as tname FROM homework h JOIN teachers t ON h.teacher_id=t.id ORDER BY due_date DESC').fetchall()
-    return render_template('teacher_dashboard.html', classes=classes, students=students, homework=homework)
+
+    teacher_id = session.get('teacher_id')
+
+    students = db.execute('''SELECT * FROM students ORDER BY class_name, roll_number''').fetchall()
+    classes = db.execute('''SELECT DISTINCT class_name FROM students ORDER BY class_name''').fetchall()
+    homework = db.execute('''SELECT * FROM homework ORDER BY created_at DESC''').fetchall()
+
+    # IMPORTANT PART
+    syllabus_list = db.execute(''' SELECT * FROM syllabus ORDER BY id DESC''').fetchall()
+    return render_template(
+        'teacher_dashboard.html',
+
+        students=students,
+        classes=classes,
+        homework=homework,
+
+        # THIS WAS MISSING
+        syllabus_list=syllabus_list
+    )
+
+
+
+
+# @app.route('/teacher/dashboard')
+# def teacher_dashboard():
+#     if session.get('role') != 'teacher': return redirect('/teacher/login')
+#     db = get_db()
+#     classes  = db.execute('SELECT DISTINCT class_name FROM students ORDER BY class_name').fetchall()
+#     students = db.execute('SELECT * FROM students ORDER BY class_name, roll_number').fetchall()
+#     homework = db.execute('SELECT h.*,t.name as tname FROM homework h JOIN teachers t ON h.teacher_id=t.id ORDER BY due_date DESC').fetchall()
+#     return render_template('teacher_dashboard.html', classes=classes, students=students, homework=homework)
 
 @app.route('/teacher/upload_marks', methods=['POST'])
 def upload_marks():
@@ -3549,51 +3581,6 @@ def delete_homework(hid):
 
 
 # TEACHER DASHBOARD
-
-@app.route('/teacher/dashboard')
-def teacher_dashboard():
-
-    if session.get('role') != 'teacher':
-        return redirect('/teacher/login')
-
-    db = get_db()
-
-    teacher_id = session.get('teacher_id')
-
-    students = db.execute('''
-        SELECT * FROM students
-        ORDER BY class_name, roll_number
-    ''').fetchall()
-
-    classes = db.execute('''
-        SELECT DISTINCT class_name
-        FROM students
-        ORDER BY class_name
-    ''').fetchall()
-
-    homework = db.execute('''
-        SELECT *
-        FROM homework
-        ORDER BY created_at DESC
-    ''').fetchall()
-
-    # IMPORTANT PART
-    syllabus_list = db.execute('''
-        SELECT *
-        FROM syllabus
-        ORDER BY id DESC
-    ''').fetchall()
-
-    return render_template(
-        'teacher_dashboard.html',
-
-        students=students,
-        classes=classes,
-        homework=homework,
-
-        # THIS WAS MISSING
-        syllabus_list=syllabus_list
-    )
 
 
 @app.route('/teacher/upload_syllabus', methods=['POST'])
